6c602fbf3f9e4e12f0368848eaa93a502d0b3b43
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2024-05-09 23:35:24 +0100

    feat(Menu): Clase Menu y método mostrarMenuPrincipal() agregados.

---
+++ src/org/empresa/duolingo/util/Menu.java
@@ -0,0 +1,19 @@
+package org.empresa.duolingo.util;
+
+public class Menu {
+
+    public static void mostrarMenuPrincipal() {
+
+        System.out.print("""
+                       \nElija una opción:
+                    1. Añadir palabra.
+                    2. Eliminar palabra.
+                    3. Buscar palabra.
+                    4. Mostrar iniciales disponibles.
+                    5. Ver palabra por inicial.
+                    Q. Salir del programa.
+                """);
+
+    }
+
+}
