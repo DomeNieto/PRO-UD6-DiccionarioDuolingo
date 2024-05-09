551d0c1928841af3e0f99d9ee03e5f34ef001b54
DomenicaNieto <usr3857@salesianos-lacuesta.net>
2024-05-09 23:21:20 +0100

    feat(Diccionario): Clase Diccionario y atributo agregado

---
+++ src/org/empresa/duolingo/diccionario/Diccionario.java
@@ -0,0 +1,11 @@
+package org.empresa.duolingo.diccionario;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Diccionario {
+
+    private Map<String, Set<String>> diccionario = new HashMap<>();
+
+}
